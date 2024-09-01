347b9a7083cbc2427648f0bc150c49ce4f829a83
VortexMB <murtaza.mzb@gmail.com>
2024-09-01 15:00:43 +0530

    Added customer model and routes.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, {Request, Response} from "express";
+import express from "express";
+import customerRouter from "./routes/customer";
 
 require("dotenv").config();
 const cors = require("cors");
@@ -10,23 +11,8 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
+app.use('/api/v1', customerRouter);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
-});
-
-app.get('/customers', async (req: Request, res: Response) => {
-
-    const customers = [
-        {
-            name: "John Doe",
-            email: "john@doe.com",
-            phone: "+91 8487996556"
-        },
-        {
-            name: "John Smith",
-            email: "john@smith.com",
-            phone: "+91 8487997557"
-        }
-    ];
-    return res.status(200).json(customers);
 });
